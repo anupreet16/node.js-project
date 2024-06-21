ad154a08024cdf22d01f73d9cb9aba0b5497d7ab
= <=>
2024-06-20 17:31:23 -0700

    [5:30 PM] Arpan Bhavsar Created the route to handle edit and delete.

--- app.js
+++ app.js
@@ -18,35 +18,89 @@ let db; // MongoDB connection reference
 
 // Connect to MongoDB
 mongodb.MongoClient.connect(mongoURI)
-  .then((client) => {
-    console.log("MongoDB connected");
-    db = client.db("student_management"); // Assign database reference to 'db' variable
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit process if MongoDB connection fails
-  });
+    .then((client) => {
+        console.log("MongoDB connected");
+        db = client.db("student_management"); // Assign database reference to 'db' variable
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+        process.exit(1); // Exit process if MongoDB connection fails
+    });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
-app.get("/students/new", (req, res)=>{
+app.get("/students", (req, res) => {
+    db.collection("students").find().toArray()
+        .then(students => {
+            res.render("index", { students });
+        })
+        .catch(err => res.status(500).json({ error: "An eroor ocurred while inserting students", details: err }));
+});
+app.get("/students/new", (req, res) => {
     res.render("add_student");
 });
 
-app.post("/students", (req, res)=> {
-    const student ={
+app.post("/students", (req, res) => {
+    const student = {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         birthdate: new Date(req.body.birthdate)
     };
 
-    db.collection("students"). insertOne(student)
-    .then(result => res.send(result))
-    .catch(err => res.status(500).json({error: "An eroor ocurred while inserting studentds", details: err}));
+    db.collection("students").insertOne(student)
+        .then(result =>  res.redirect("/students"))
+        .catch(err => res.status(500).json({ error: "An eroor ocurred while inserting students", details: err }));
 });
 
+app.get("/students/:id/edit", (req, res) => {
+    const { id } = req.params;
+    const objectID = new mongodb.ObjectId(id);
+    db.collection("students").findOne({_id: objectID })
+        .then(student => {
+            if(!student){
+               return res.status(404).json({ error: "student not found", details: err });
+            }
+            res.render("edit_student copy", { student });
+        })
+        .catch(err => res.status(500).json({ error: "An eroor ocurred while inserting students", details: err }));
+});
+
+
+app.put("/students/:id", (req, res) => {
+    const { id } = req.params;
+    const objectId = new mongodb.ObjectId(id);
+  
+    const updateStudent = {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      birthdate: new Date(req.body.birthdate)
+    };
+  
+    db.collection("students").updateOne({ _id: objectId }, { $set: updateStudent })
+    .then(result => {
+      if(result.matchedCount === 0) {
+        return res.status(404).json({error: "Student not found.", detail: err});
+      }
+      res.redirect("/students");
+    })
+    .catch(err => res.status(500).json({error: "An error occurred while updating student", detail: err}));
+  });
+  
+  app.delete("/students/:id", (req, res) => {
+    const { id } = req.params;
+    const objectId = new mongodb.ObjectId(id);
+  
+    db.collection("students").deleteOne({ _id: objectId })
+    .then(result => {
+      if(result.deletedCount === 0) {
+        return res.status(404).json({error: "Student not found.", detail: err});
+      }
+      res.redirect("/students");
+    })
+    .catch(err => res.status(500).json({error: "An error occurred while deleting student", detail: err}));
+  });
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+    console.log(`Server started at http://localhost:${port}`);
 });
